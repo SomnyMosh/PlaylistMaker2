f9a7a5bc2d14b15502d56a403e06579869c2680d
SomnyMosh <workshopofrenen@gmail.com>
2023-05-29 19:47:22 +0800

    спринт 9, темы 1-2, частично тема 3

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,8 +1,16 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.view.Window
+import android.widget.SearchView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.security.AccessController.getContext
+
 
 class SearchActivity : AppCompatActivity() {
 
@@ -11,5 +19,16 @@ class SearchActivity : AppCompatActivity() {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         supportActionBar?.hide()
         setContentView(R.layout.activity_search)
+        val getBack = findViewById<TextView>(R.id.searchArrowBackButton)
+        val searchView = findViewById<SearchView>(R.id.search_view)
+        val id: Int = searchView.context.resources.getIdentifier("android:id/search_src_text", null, null)
+        searchView.findViewById<TextView>(id).setTextColor(Color.parseColor("#AEAFB4"))
+        getBack.setOnClickListener {
+            finish()
+        }
+    }
+    private fun isDarkModeOn(): Boolean {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,20 +1,70 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.Window
+import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         supportActionBar?.hide()
         setContentView(R.layout.activity_settings)
+        val sup = findViewById<TextView>(R.id.support)
+        val share = findViewById<TextView>(R.id.share)
+        val userA = findViewById<TextView>(R.id.userAgreement)
         val getBack = findViewById<TextView>(R.id.arrowBackButton)
+        val switchM : Switch =  findViewById(R.id.switch1)
+        var sharedPreferences = getSharedPreferences("save", MODE_PRIVATE)
+        switchM.setChecked(sharedPreferences.getBoolean("value",true))
+        switchM.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                val editor = getSharedPreferences("save", MODE_PRIVATE).edit()
+                editor.putBoolean("value", true)
+                editor.apply()
+                switchM.setChecked(true)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }else{
+                val editor = getSharedPreferences("save", MODE_PRIVATE).edit()
+                editor.putBoolean("value", false)
+                editor.apply()
+                switchM.setChecked(false)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
         getBack.setOnClickListener {
             finish()
+        }
+        sup.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:")
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val title = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val email = "WorkshopOfRenen@gmail.com"
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, title)
+            emailIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(emailIntent)
+        }
+        share.setOnClickListener {
+            val shareIntent= Intent()
+            shareIntent.action=Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_TEXT,"https://practicum.yandex.ru/android-developer/")
+            shareIntent.type="text/plain"
+            startActivity(Intent.createChooser(shareIntent,"Share To:"))
+        }
+        userA.setOnClickListener {
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(intent)
         }
     }
 }
